f41ae6a34201beb3bb076ae246c8e21252a4a6db
peterLERNSAX <i21kobanpe@bszetdd.lernsax.de>
2023-04-24 12:24:30 +0200

    feat(ticket): index-view

---
+++ ticketsystem/ticketwand/urls.py
@@ -0,0 +1,11 @@
+"""
+urls
+"""
+from django.urls import path
+
+from . import views
+
+urlpatterns =[
+    path("",views.IndexView.as_view(),name="index-view"),
+
+]
--- ticketsystem/ticketwand/views.py
+++ ticketsystem/ticketwand/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpRequest,HttpResponse
 
 # Create your views here.
+
+class IndexView(View):
+    """
+    Index
+    """ 
+
+    def get(self,request:HttpRequest)->HttpResponse:
+        """
+        get
+        """
+        return render(request,"ticketwand/index.html") 
