12d1291e79088b334ca8b256b7705e12769b342e
peterLERNSAX <i21kobanpe@bszetdd.lernsax.de>
2023-04-25 12:24:35 +0200

    feat(ticket): create ticket

--- ticketsystem/ticketwand/models.py
+++ ticketsystem/ticketwand/models.py
@@ -1,3 +1,28 @@
+"""
+Models
+"""
+
 from django.db import models
+from django.contrib.auth.models import User # new
 
 # Create your models here.
+
+class Ticket(models.Model):
+    """Model for Ticket"""
+
+    status_choice = (
+    ("in Bearbeitung", "in Bearbeitung"),
+    ("nicht lösbar", "nicht lösbar"),
+    ("reserveiert", "reserviert"),
+)
+
+    name = models.CharField(max_length=250,null=False,default="")
+    text = models.TextField(null=False,default="")
+    since = models.DateTimeField(null=False)
+    creator_email = models.EmailField(null=False)
+    status = models.CharField(null=True,choices=status_choice,max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    is_active = models.BooleanField(null=False,default=True)
+    labels = models.TextField(null=True)
+
+
--- ticketsystem/ticketwand/urls.py
+++ ticketsystem/ticketwand/urls.py
@@ -8,5 +8,6 @@ from . import views
 urlpatterns =[
     path("",views.IndexView.as_view(),name="index-view"),
     path("login/",views.LoginView.as_view(),name="login-view"),
+    path("ticket/erstellen/",views.CreateTicketView.as_view(),name="create-ticket-view"),
 
 ]
